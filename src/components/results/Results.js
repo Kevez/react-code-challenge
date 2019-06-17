066dc8e1fc678f1800e49c4df9ecc254a90ab09c
Kevez <kbrady08@gmail.com>
2019-06-17 10:02:02 +0100

    Change to function

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -1,32 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Results extends Component {
-  render() {
-    let tableData = this.props.results.map(result => {
-      return (
-        <tr key={result.id}>
-          <td className="col-1">{result.id}</td>
-          <td className="col-2">{result.name}</td>
-          <td className="col-3">{result.main_address.postcode}</td>
-          <td className="col-4">{result.review_rating.toFixed(2)}</td>
-        </tr>
-      );
-    });
-  
+function Results(props) {
+  let tableData = props.results.map(result => {
     return (
-      <table>
-        <tbody>
-        <tr>
-          <th className="col-1">Id</th>
-          <th className="col-2">Name</th>
-          <th className="col-3">Postcode</th>
-          <th className="col-4">Review Rating</th>
-        </tr>
-          {tableData}
-        </tbody>
-      </table>
+      <tr key={result.id}>
+        <td className="col-1">{result.id}</td>
+        <td className="col-2">{result.name}</td>
+        <td className="col-3">{result.main_address.postcode}</td>
+        <td className="col-4">{result.review_rating.toFixed(2)}</td>
+      </tr>
     );
-  }
+  });
+  
+  return (
+    <table>
+      <tbody>
+      <tr>
+        <th className="col-1">Id</th>
+        <th className="col-2">Name</th>
+        <th className="col-3">Postcode</th>
+        <th className="col-4">Review Rating</th>
+      </tr>
+      {tableData}
+      </tbody>
+    </table>
+  );
+  
 }
 
 export default Results;
